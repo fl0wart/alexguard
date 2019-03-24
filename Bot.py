8a71a9a16a6469c92cbce8da25a37a540c74eaac
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-24 20:37:23 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
 import asyncio
-import colorsys
-import random
     
 bot=commands.Bot(command_prefix='ap!')
 @bot.event
@@ -19,13 +17,5 @@ async def say(ctx, *args):
     mesg = ' '.join(args)
     await bot.delete_message(ctx.message)
     return await bot.say(mesg)
-
-@bot.command(pass_context = True)
-@commands.has_permissions(administrator=True) 
-async def announce(ctx, channel: discord.Channel=None, *, msg: str):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    embed=discord.Embed(title="HIGHSCAPE - Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
-    await client.send_message(channel, embed=embed)
-    await client.delete_message(ctx.message)
     
 bot.run('NTU4NTQyODkwMzA2MTA5NDUy.D3lPpA.yBZoL0UCcBb9ilQUuDVo1XaGJoU')
