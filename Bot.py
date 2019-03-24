2c49e175e7db666c8d15f8f8db1a44ca8ec7e166
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-24 19:53:53 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,12 +7,12 @@ bot=commands.Bot(command_prefix='ap!')
 async def on_ready():
     print('Logged in as '+bot.user.name+' (ID:'+bot.user.id+') | Connected to '+str(len(bot.servers))+' servers | Connected to '+str(len(set(bot.get_all_members())))+' users')
     print('--------')
-    await Bot.change_presence(game=discord.Game(name="with portals! 🌀", type=1))
+    await bot.change_presence(game=discord.Game(name="with portals! 🌀", type=0))
 
 bot.remove_command('help')
 
 @bot.command(pass_context = True)
-@commands.has_permissions(manage_messages = True)
+@commands.has_permissions(administrator = True)
 async def say(ctx, *args):
     mesg = ' '.join(args)
     await bot.delete_message(ctx.message)
